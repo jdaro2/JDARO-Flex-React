3d7c9d0d228dd57f6a78d5abde7ad7684ecab6b1
jdaro2 <61673619+jdaro2@users.noreply.github.com>
2024-08-01 19:55:22 +1000

    Initial Commit
    Current progress of following tutorial: https://www.youtube.com/watch?v=eh8mpiN-7bA&t=251s

---
+++ movie-app/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Box, Container, Flex } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <Box py="4" mb="2">
+      <Container maxW={"container.xl"}>
+        <Flex justifyContent={"space-between"}>
+          <Link to="/">
+            <Box
+              fontSize={"2xl"}
+              fontWeight={"bold"}
+              color={"red"}
+              letterSpacing={"widest"}
+              fontFamily={"mono"}
+            >
+              JDARO Flex
+            </Box>
+          </Link>
+
+          {/* Desktop */}
+          <Flex gap="4" alignItems={"center"}>
+            <Link to="/">Home</Link>
+            <Link to="/movies">Movies</Link>
+            <Link to="/shows">TV Shows</Link>
+            <Link to="/search">Search</Link>
+          </Flex>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+export default Navbar;
