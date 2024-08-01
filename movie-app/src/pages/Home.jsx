daa3e52889af7d80d6739ff3d8bb50d1e109d677
jdaro2 <61673619+jdaro2@users.noreply.github.com>
2024-08-01 20:15:07 +1000

    TMDB Api Implemented
    Started the implementation of TMDB API into the website

--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import { Container, Heading } from "@chakra-ui/react";
+import { fetchTrending } from "../services/api";
 
 const Home = () => {
+  useEffect(() => {
+    fetchTrending("day")
+      .then((res) => {
+        console.log(res, "res");
+      })
+      .catch((err) => {
+        console.log(err, "err");
+      });
+  }, []);
+
   return (
     <Container maxW={"container.xl"}>
       <Heading as="h2" fontSize={"md"} textTransform={"uppercase"}>
