6521b4a37107ec0434403b85e37efc0a110ffde3
jdaro2 <61673619+jdaro2@users.noreply.github.com>
2024-08-02 20:55:20 +1000

    Completed Banner of Details Page

--- movie-app/src/pages/DetailsPage.jsx
+++ movie-app/src/pages/DetailsPage.jsx
@@ -1,14 +1,21 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {
+  Badge,
   Box,
+  Button,
+  CircularProgress,
+  CircularProgressLabel,
   Container,
   Flex,
   Heading,
   Image,
   Spinner,
+  Text,
 } from "@chakra-ui/react";
 import { fetchDetails, imagePath, imagePathOriginal } from "../services/api";
+import { CalendarIcon, CheckCircleIcon, SmallAddIcon } from "@chakra-ui/icons";
+import { ratingToPercentage, resolveRatingColor } from "../utils/helpers";
 
 const DetailsPage = () => {
   const router = useParams();
@@ -40,6 +47,8 @@ const DetailsPage = () => {
   }
 
   const title = details?.title || details?.name;
+  const releaseDate =
+    type === "tv" ? details?.first_air_date : details?.release_date;
 
   return (
     <Box>
@@ -66,7 +75,78 @@ const DetailsPage = () => {
               src={`${imagePath}/${details?.poster_path}`}
             />
             <Box>
-              <Heading fontSize={"3xl"}>{title}</Heading>
+              <Heading fontSize={"3xl"}>
+                {title}{" "}
+                <Text as={"span"} fontWeight={"normal"} color="grey.400">
+                  {new Date(releaseDate).getFullYear()}
+                </Text>
+              </Heading>
+
+              <Flex alignItems={"center"} gap="4" nt={1} mb={5}>
+                <Flex alignItems={"center"}>
+                  <CalendarIcon mr={2} color={"gray.400"} />
+                  <Text fontSize={"small"}>
+                    {new Date(releaseDate).toLocaleDateString("en-US")} {"US"}
+                  </Text>
+                </Flex>
+              </Flex>
+
+              <Flex alignItems={"center"} gap={"4"}>
+                <CircularProgress
+                  value={ratingToPercentage(details?.vote_average)}
+                  bg={"gray.800"}
+                  borderRadius={"full"}
+                  p={"0.5"}
+                  size={"70px"}
+                  color={resolveRatingColor(details?.vote_average)}
+                  thickness={"6px"}
+                >
+                  <CircularProgressLabel fontSize={"lg"}>
+                    {ratingToPercentage(details?.vote_average)}{" "}
+                    <Box as="span" fontSize={"10px"}>
+                      %
+                    </Box>
+                  </CircularProgressLabel>
+                </CircularProgress>
+                <Text display={{ base: "none", md: "initial" }}>
+                  User Score
+                </Text>
+                <Button
+                  display={"none"}
+                  leftIcon={<CheckCircleIcon />}
+                  colorScheme="purple"
+                  variant={"outline"}
+                  onClick={() => console.log("click")}
+                >
+                  In watchlist
+                </Button>
+                <Button
+                  leftIcon={<SmallAddIcon />}
+                  variant={"outline"}
+                  onClick={() => console.log("click")}
+                >
+                  Add to watchlist
+                </Button>
+              </Flex>
+              <Text
+                color={"gray.400"}
+                fontSize={"sm"}
+                fontStyle={"italic"}
+                my="5"
+              >
+                {details?.tagline}
+              </Text>
+              <Heading fontSize={"xl"} mb="3">
+                Overview
+              </Heading>
+              <Text fontSize={"md"} mb={"3"}>
+                {details?.overview}
+              </Text>
+              <Flex mt="6" gap="2">
+                {details?.genres?.map((genre) => (
+                  <Badge key={genre?.key}>{genre?.name}</Badge>
+                ))}
+              </Flex>
             </Box>
           </Flex>
         </Container>
