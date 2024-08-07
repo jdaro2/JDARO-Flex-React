c5854ae0f098821d35226705c2059a200c2ac07f
jdaro2 <61673619+jdaro2@users.noreply.github.com>
2024-08-07 17:59:03 +1000

    More mobile accessibility

--- movie-app/src/components/VideoComponent.jsx
+++ movie-app/src/components/VideoComponent.jsx
@@ -1,15 +1,20 @@
 import PropTypes from "prop-types";
+import React from "react";
+import "../index.css";
+import { Box } from "@chakra-ui/react";
 
 const VideoComponent = ({ id, small }) => {
   return (
-    <iframe
-      width="100%"
-      height={small ? "150" : "500"}
-      src={`https://www.youtube.com/embed/${id}`}
-      alt=""
-      title="YouTube video player"
-      allowFullScreen
-    ></iframe>
+    <Box className="videoWrapper">
+      <iframe
+        width="100%"
+        height={small ? "150" : "500"}
+        src={`https://www.youtube.com/embed/${id}`}
+        alt=""
+        title="YouTube video player"
+        allowFullScreen
+      ></iframe>
+    </Box>
   );
 };
 
--- movie-app/src/pages/movies/Movies.jsx
+++ movie-app/src/pages/movies/Movies.jsx
@@ -57,7 +57,7 @@ const Movies = () => {
 
       <Grid
         templateColumns={{
-          base: "1fr",
+          base: "repeat(2, 1fr)",
           sm: "repeat(2, 1fr)",
           md: "repeat(4, 1fr)",
           lg: "repeat(5, 1fr)",
